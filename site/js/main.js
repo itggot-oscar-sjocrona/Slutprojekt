ca7575e961d8eab2ee1b08448b4813c26c49bf2e
itggot-oscar-sjocrona <oscar.sjocrona@itggot.se>
2017-03-28 11:54:18 +0200

    css/js/html

---
+++ site/js/main.js
@@ -0,0 +1,6 @@
+function display_message(element) {
+  var temp = document.querySelector(element);
+  temp.classList.add("hidden");
+  var temp2 = document.querySelector('#message')
+  temp2.classList.add("message")
+}
